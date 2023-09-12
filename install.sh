ccaaf94f6ab8476a4feef034aad95bfbb1745304
Alezer0Dev <142743813+Alezer0Dev@users.noreply.github.com>
2023-09-12 16:45:41 +0200

    Fix sudo thing

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 clear
-apt update
+sudo apt update
 clear
-apt upgrade -y
-apt install -y git python3 build-essential
-apt install -y ca-certificates curl gnupg
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+sudo apt upgrade -y
+sudo apt install -y git python3 build-essential
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-apt update
-apt install -y nodejs
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install -y nodejs
 git clone https://github.com/spacebarchat/server.git
 cd server
 npm i -y
